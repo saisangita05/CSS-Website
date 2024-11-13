d36da3ed47dadaa0b7cc703b3c8957c9abc24ea2
The Dakhinia <wwww.swastiknayak.d@gmail.com>
2024-11-13 20:47:25 +0530

    Second commit after final design

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNAv";
+import { Navbar } from "@/components/ui/Navbar";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -8,11 +8,8 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          { name: 'Home', link: '/', icon: <FaHome /> }
-        ]} />
+        <Navbar />
         <Hero />
-        <Grid/>
       </div>
     </main>
   );
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/Navbar-Menu";
+import { cn } from "@/lib/utils";
+
+export function Navbar({ className }: { className?: string }) {
+    const [active, setActive] = useState<string | null>(null);
+    return (
+        <div
+            className={cn("fixed top-5 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <HoveredLink href="/">Home</HoveredLink>
+                <MenuItem setActive={setActive} active={active} item="About">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/css">CSS</HoveredLink>
+                        <HoveredLink href="/ddx">DDX</HoveredLink>
+                        <HoveredLink href="/samarpan">Samarpan</HoveredLink>
+                        <HoveredLink href="/theatre">Theatre</HoveredLink>
+                    </div>
+                </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Events">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/upcoming">Upcoming</HoveredLink>
+                        <HoveredLink href="/gallery">Gallery</HoveredLink>
+                    </div>
+                </MenuItem>
+                <HoveredLink href="/gallery">Gallery</HoveredLink>
+            </Menu>
+        </div>
+    );
+}
